91399d5bc5209b4884b156b8c532ca7f2d36d5dc
sakshiS6 <sakshimishra2004545@gmail.com>
2025-02-15 07:06:49 +0530

    Updated the Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,25 @@
-import { IconMessageChatbotFilled } from '@tabler/icons-react'
+import { IconMenu2, IconMessageChatbotFilled, IconMoon, IconX } from '@tabler/icons-react'
 import Link from 'next/link'
-import React from 'react'
+import React, { useRef } from 'react'
 
 const Navbar = () => {
+
+  const sideMenuRef = useRef();
+
+  const openMenu =()=>{
+    sideMenuRef.current.style.transform='translate(-16rem)'
+  }
+  const closeMenu =()=>{
+    sideMenuRef.current.style.transform='translate(16rem)'
+  }
+
   return (
     <>
+    {/* I have to put the bg image */}
+    <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
+      <img src="" alt="" className='w-full' /> 
+    </div>
+
     <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50'>
         <Link href="#top">
             <img src="/Icon.png" alt="Icon" className='w-10 cursor-pointer mr-14' />
@@ -18,9 +33,32 @@ const Navbar = () => {
           <li><Link href="#contact" className='font-Ovo'>Contact me</Link></li>
         </ul>
 
-        <div>
+        <div className='flex items-center gap-4'>
+          
+          {/* Dark theme icon - IconMoonFilled */}
+          <button><IconMoon className='w-6'/></button>
+
+
           <Link href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-14'>Contact <IconMessageChatbotFilled className='w-6'/></Link>
+
+          <button className='block md:hidden ml-3 lg:hidden' onClick={openMenu}>
+            <IconMenu2 className='w-6'/>
+          </button>
         </div>
+
+        {/* Mobile menu */}
+        <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 trnsition duration-500'>
+
+          <div className='absolute right-6 top-6' onClick={closeMenu}>
+            <IconX className='w-8 cursor-pointer'/>
+          </div>
+          <li><Link href="#top" className='font-Ovo' onClick={closeMenu} >Home</Link></li>
+          <li><Link href="#about" className='font-Ovo' onClick={closeMenu} >About me</Link></li>
+          <li><Link href="#services" className='font-Ovo' onClick={closeMenu} >Services</Link></li>
+          <li><Link href="#work" className='font-Ovo' onClick={closeMenu} >My Work</Link></li>
+          <li><Link href="#contact" className='font-Ovo' onClick={closeMenu} >Contact me</Link></li>
+        </ul>
+
     </nav>
     </>
   )
