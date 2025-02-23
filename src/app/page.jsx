2c44fc06ed9b2257368f469df272c07e72282359
sakshiS6 <sakshimishra2004545@gmail.com>
2025-02-23 15:04:33 +0530

    Updates

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,6 @@
 'use client'
 import About from '@/components/About'
+import ContactUs from '@/components/ContactUs'
 import Header from '@/components/Header'
 import Navbar from '@/components/Navbar'
 import Services from '@/components/Services'
@@ -14,6 +15,7 @@ const Home = () => {
     <About/>
     <Services/>
     <Work/>
+    <ContactUs/>
     </>
   )
 }
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,3 +1,5 @@
+import { IconArrowRight, IconSend } from "@tabler/icons-react";
+import Link from "next/link";
 import React from "react";
 
 const Work = () => {
@@ -13,32 +15,65 @@ const Work = () => {
         recusandae nihil mollitia accusamus assumenda! Aspernatur et
         voluptatibus amet?
       </p>
-      <div className="grid grid-cols-auto gap-6 my-10 ">
-        <div className="aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group">
-          <img
-            src="https://www.growth-rocket.com/wp-content/uploads/2023/02/Front-end-vs-back-end-developers-rectangle.jpg"
-            alt=""
-          />
-          <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
-            <div>
-              <h3 className="font-semibold">Frontend Project</h3>
-              <h4 className="text-sm text-gray-700 ">Web design</h4>
-            </div>
-            <div>
-              <h3 className="font-semibold">Frontend Project</h3>
-              <h4 className="text-sm text-gray-700 ">Web design</h4>
-            </div>
-            <div>
-              <h3 className="font-semibold">Frontend Project</h3>
-              <h4 className="text-sm text-gray-700 ">Web design</h4>
-            </div>
-            <div>
-              <h3 className="font-semibold">Frontend Project</h3>
-              <h4 className="text-sm text-gray-700 ">Web design</h4>
+      <div className="grid grid-cols-auto my-10 gap-5">
+        
+          {/* Project-1 */}
+          <div
+            className="aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group "
+            style={{
+              backgroundImage: `url(https://www.growth-rocket.com/wp-content/uploads/2023/02/Front-end-vs-back-end-developers-rectangle.jpg)`,
+            }}
+          >
+            <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7 ">
+              <h2 className="font-semibold ">Frontend</h2>
+              <h3 className=" text-sm text-gray-700">web design</h3>
+              <IconSend className="border rounded-full border-black w-fit aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-200 transition"></IconSend>
             </div>
           </div>
-        </div>
+          {/* Project-2 */}
+          <div
+            className="aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group "
+            style={{
+              backgroundImage: `url(https://www.growth-rocket.com/wp-content/uploads/2023/02/Front-end-vs-back-end-developers-rectangle.jpg)`,
+            }}
+          >
+            <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7 ">
+              <h2 className="font-semibold ">Frontend</h2>
+              <h3 className=" text-sm text-gray-700">web design</h3>
+              <IconSend className="border rounded-full border-black w-fit aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-200 transition"></IconSend>
+            </div>
+          </div>
+          {/* Project-3 */}
+          <div
+            className="aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group "
+            style={{
+              backgroundImage: `url(https://www.growth-rocket.com/wp-content/uploads/2023/02/Front-end-vs-back-end-developers-rectangle.jpg)`,
+            }}
+          >
+            <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7 ">
+              <h2 className="font-semibold ">Frontend</h2>
+              <h3 className=" text-sm text-gray-700">web design</h3>
+              <IconSend className="border rounded-full border-black w-fit aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-200 transition"></IconSend>
+            </div>
+          </div>
+          {/* Project-4 */}
+          <div
+            className="aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group "
+            style={{
+              backgroundImage: `url(https://www.growth-rocket.com/wp-content/uploads/2023/02/Front-end-vs-back-end-developers-rectangle.jpg)`,
+            }}
+          >
+            <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7 ">
+              <h2 className="font-semibold ">Frontend</h2>
+              <h3 className=" text-sm text-gray-700">web design</h3>
+              <IconSend className="border rounded-full border-black w-fit aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-200 transition"></IconSend>
+            </div>
+          </div>
+      
       </div>
+      <Link href="" className="w-max flex itmes-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-lighthover duration-500">
+        Show more <IconArrowRight/>
+      </Link>
     </div>
   );
 };
