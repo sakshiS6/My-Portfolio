c120d2156e7a0357f6ff73537b5677330aac35a8
sakshiS6 <sakshimishra2004545@gmail.com>
2025-02-23 22:45:18 +0530

    Updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,4 @@
+import { IconMailFilled } from '@tabler/icons-react'
 import Link from 'next/link'
 import React from 'react'
 
@@ -5,13 +6,13 @@ const Footer = () => {
   return (
     <div className='mt-20'>
         <div className='text-center'>
-            <img src="" alt="Logo" className='w-36 mx-auto mb-2' />
+            <img src="https://signature.freefire-name.com/img.php?f=11&t=Sakshi" alt="Logo" className='w-1/3 mx-auto' />
             <div className='w-max flex items-center gap-2 mx-auto'>
-                <img src="" alt="mail-icon" className='w-6' />sakshimishra@gmail.com
+                <IconMailFilled className='w-6' />sakshimishra@gmail.com
             </div>
         </div>
         <div className='text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6'>
-            <p className='font-semibold'>2025 Sakshi Mishra. All rights reserved.</p>
+            <p className='font-semibold'>© 2025 Sakshi Mishra. All rights reserved.</p>
             <ul className='flex items-center gap-10 justify-center mt-4 font-semibold sm:mt-0'>
                 <li><Link target='_blank' href="https://github.com/sakshiS6">Github</Link></li>
                 <li><Link target='_blank' href="https://www.linkedin.com/feed/">LinkedIn</Link></li>
