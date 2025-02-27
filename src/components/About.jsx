10aa86329ff7a4d8e51da042450b611e90a202b9
sakshiS6 <sakshimishra2004545@gmail.com>
2025-02-27 22:13:20 +0530

    Updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,31 +5,84 @@ import {
   IconCode,
 } from "@tabler/icons-react";
 import React from "react";
+import { motion } from "motion/react";
 
 const About = (isDarkMode) => {
   return (
-    <div id="about" className="w-full px-[12%] py-10 scroll-mt-20">
-      <h4 className="text-center mb-2 text-lg font-Nunito">Introduction</h4>
-      <h2 className="text-center text-5xl font-Nunito">About me</h2>
-      <div className="flex w-full flex-col lg:flex-row items-center gap-20 my-12">
-        <div className="w-64 sm:w-80 rounded-3xl max-w-none">
-          <img src="MyImage.jpg" alt="user-image" className=" rounded-3xl" />
-        </div>
-        <div className="flex1">
+    <motion.div
+      id="about"
+      className="w-full px-[12%] py-10 scroll-mt-20"
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 2 }}
+      transition={{ duration: 1 }}
+    >
+      <motion.h4
+        initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1, delay: 0.3 }}
+        className="text-center mb-2 text-lg font-Nunito"
+      >
+        Introduction
+      </motion.h4>
+      <motion.h2
+        initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.5 }}
+        className="text-center text-5xl font-Nunito"
+      >
+        About me
+      </motion.h2>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.8 }}
+        className="flex w-full flex-col lg:flex-row items-center gap-20 my-12"
+      >
+        <motion.div
+          initial={{ opacity: 0, scale: 0.9 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.6 }}
+          className="w-80 sm:w-80 rounded-3xl max-w-none"
+        >
+          <img
+            src="MyImage.jpg"
+            alt="user-image"
+            className=" rounded-3xl w-80"
+          />
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 0.6, delay: 0.8 }}
+          className="flex1"
+        >
           <p>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam
             suscipit facere facilis necessitatibus dolorem, tenetur, distinctio
             nam delectus magni similique sequi quia officia, quo minus. Quasi.
           </p>
-          <div className="grid grid-cols-1 mt-6 sm:grid-cols-3 gap-6 max-w-2xl">
-            <div className="border border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-lighthover hover:-translate-y-1 duration-500 hover:shadow-black dark:border-white dark:hover:shadow-white dark:hover:bg-darkhover/50">
+          <motion.div
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 0.8, delay: 1 }}
+            className="grid grid-cols-1 mt-6 sm:grid-cols-3 gap-6 max-w-2xl"
+          >
+            <motion.div
+              whileInView={{ scale: 1.05 }}
+              className="border border-gray-400 rounded-xl m-2 p-6 cursor-pointer hover:bg-lighthover hover:-translate-y-1 duration-500 hover:shadow-black dark:border-white dark:hover:shadow-white dark:hover:bg-darkhover/50"
+            >
               <IconCode size={66} />
               <h3 className="my-4 font-semibold text-gray-700 text-lg dark:text-white">
                 Languages
               </h3>
-              <p className="text-gray-700 dark:text-white/80">HTML , CSS JS , MERN</p>
-            </div>
-            <div className="border border-gray-400 rounded-xl m-2 px-6 py-6 cursor-pointer hover:bg-lighthover hover:-translate-y-1 duration-500 hover:shadow-black dark:border-white dark:hover:shadow-white dark:hover:bg-darkhover/50">
+              <p className="text-gray-700 dark:text-white/80">
+                HTML , CSS JS , MERN
+              </p>
+            </motion.div>
+            <motion.div
+              whileInView={{ scale: 1.05 }}
+              className="border border-gray-400 rounded-xl m-2 px-6 py-6 cursor-pointer hover:bg-lighthover hover:-translate-y-1 duration-500 hover:shadow-black dark:border-white dark:hover:shadow-white dark:hover:bg-darkhover/50"
+            >
               <IconBook size={66} />
               <h3 className="my-4 font-semibold text-gray-700 text-lg dark:text-white">
                 Education
@@ -37,48 +90,59 @@ const About = (isDarkMode) => {
               <p className="text-gray-700 dark:text-white/80">
                 Btech in computer science and design
               </p>
-            </div>
-            <div className="border border-gray-400 rounded-xl m-2 px-6 py-6 cursor-pointer hover:bg-lighthover hover:-translate-y-1 duration-500  hover:shadow-black dark:border-white dark:hover:shadow-white dark:hover:bg-darkhover/50">
+            </motion.div>
+            <motion.div
+              whileInView={{ scale: 1.05 }}
+              className="border border-gray-400 rounded-xl m-2 px-6 py-6 cursor-pointer hover:bg-lighthover hover:-translate-y-1 duration-500  hover:shadow-black dark:border-white dark:hover:shadow-white dark:hover:bg-darkhover/50"
+            >
               <IconBriefcase size={66} />
               <h3 className="my-4 font-semibold text-gray-700 text-lg dark:text-white">
                 Projects
               </h3>
-              <p className="text-gray-700 dark:text-white/80">Poll Maker website</p>
-            </div>
-          </div>
-          <h4 className="my-6 text-gray-700 text-2xl font-semibold font-Nunito dark:text-white/90">
+              <p className="text-gray-700 dark:text-white/80">
+                Poll Maker website
+              </p>
+            </motion.div>
+          </motion.div>
+          <motion.h4
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 1.3 }}
+            className="my-6 text-gray-700 text-2xl font-semibold font-Nunito dark:text-white/90"
+          >
             Tools I use
-          </h4>
+          </motion.h4>
           <ul className="flex items-center gap-3 sm:gap-5">
-            <img
+            <motion.img
+            
               src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg"
               alt="VS Code Logo"
               className="w-12 p-2 flex items-center justify-center sm:14 aspect-square border border-gray-400 rounded-lg cursor-pointer hover:-translate-y-1 duration-500"
             />
-            <img
+            <motion.img
               src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg"
               alt="VS Code Logo"
               className="w-12 p-2 flex items-center justify-center sm:14 aspect-square border border-gray-400 rounded-lg cursor-pointer hover:-translate-y-1 duration-500"
             />
-            <img
+            <motion.img
               src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg"
               alt="VS Code Logo"
               className="w-12 p-2 flex items-center justify-center sm:14 aspect-square border border-gray-400 rounded-lg cursor-pointer hover:-translate-y-1 duration-500"
             />
-            <img
+            <motion.img
               src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg"
               alt="VS Code Logo"
               className="w-12 p-2 flex items-center justify-center sm:14 aspect-square border border-gray-400 rounded-lg cursor-pointer hover:-translate-y-1 duration-500"
             />
-            <img
+            <motion.img
               src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg"
               alt="VS Code Logo"
               className="w-12 p-2 flex items-center justify-center sm:14 aspect-square border border-gray-400 rounded-lg cursor-pointer hover:-translate-y-1 duration-500"
             />
           </ul>
-        </div>
-      </div>
-    </div>
+        </motion.div>
+      </motion.div>
+    </motion.div>
   );
 };
 
